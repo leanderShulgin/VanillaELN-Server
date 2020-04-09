55a766e42bc66cf1e3747938466c3ace5e1a2ed4
Lean <leandrohornos@gmail.com>
2020-04-08 21:42:53 -0300

    creo las primeras rutas

--- server/app.js
+++ server/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+// Parseo JSON
+app.use(express.json());
+
+// Headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// Rutas
+app.use("/", require("./routes/index"))
+app.use("/api", require("./routes/api"))
+
+// Conexion
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Bienvenido al servidor");
+  res.send("Bienvenido al la api");
 });
 
 module.exports = router;
